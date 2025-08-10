66d315f820ef856f80b06b3f6ce70893c76e78f5
Ulysses3110 <ulysses3110@gmail.com>
2025-08-10 01:17:49 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 
@@ -24,8 +23,8 @@ ano_disponiveis = sorted(df['Ano'].unique())
 ano_selecionados = st.sidebar.multiselect("Ano", ano_disponiveis, default=ano_disponiveis)
 
 # Filtro por CGCE
-cgce_disponiveis = sorted(df['Descrição CGCE Nível 1'].unique())
-cgce_selecionados = st.sidebar.multiselect("Tipo de Contrato", cgce_disponiveis, default=cgce_disponiveis)
+cgce_disponiveis = sorted(df['Código CGCE Nível 1'].unique())
+cgce_selecionados = st.sidebar.multiselect("Código CGCE Nível 1", cgce_disponiveis, default=cgce_disponiveis)
 
 # Filtro por ISIC
 isic_disponiveis = sorted(df['Descrição ISIC Seção'].unique())
@@ -40,7 +39,7 @@ cuci_selecionados = st.sidebar.multiselect("Descrição CUCI Seção", cuci_disp
 df_filtrado = df[
     (df['Fluxo'].isin(fluxo_selecionados)) &
     (df['Ano'].isin(ano_selecionados)) &
-    (df['Descrição CGCE Nível 1'].isin(cgce_selecionados)) &
+    (df['Código CGCE Nível 1'].isin(cgce_selecionados)) &
     (df['Descrição ISIC Seção'].isin(isic_selecionados)) &
     (df['Descrição CUCI Seção'].isin(cuci_selecionados))
 ]
@@ -81,7 +80,7 @@ with col_graf1:
             y='Descrição NCM',
             orientation='h',
             title="Top 10 NCM por valor total",
-            labels={'Valor US$ FOB': 'Valor comércio anual (USD)', 'Descrição NCM': ''}
+            labels={'Valor US$ FOB': 'Valor anual (USD)', 'Descrição NCM': ''}
         )
         grafico_ncm.update_layout(title_x=0.1, yaxis={'categoryorder':'total ascending'})
         st.plotly_chart(grafico_ncm, use_container_width=True)
@@ -90,14 +89,14 @@ with col_graf1:
 
 with col_graf2:
     if not df_filtrado.empty:
-        top_cgce = df_filtrado.groupby('Descrição CGCE Nível 1')['Valor US$ FOB'].sum().nlargest(10).sort_values(ascending=True).reset_index()
+        top_cgce = df_filtrado.groupby('Código CGCE Nível 1')['Valor US$ FOB'].sum().nlargest(5).sort_values(ascending=True).reset_index()
         grafico_cgce = px.bar(
             top_cgce,
             x='Valor US$ FOB',
-            y='Descrição CGCE Nível 1',
+            y='Código CGCE Nível 1',
             orientation='h',
-            title="Top 10 CGCE por valor total",
-            labels={'Valor US$ FOB': 'Valor comércio anual (USD)', 'Descrição CGCE Nível 1': ''}
+            title="Top 5 CGCE por valor total",
+            labels={'Valor US$ FOB': 'Valor anual (USD)', 'Código CGCE Nível 1': ''}
         )
         grafico_cgce.update_layout(title_x=0.1, yaxis={'categoryorder':'total ascending'})
         st.plotly_chart(grafico_cgce, use_container_width=True)
@@ -115,7 +114,7 @@ with col_graf3:
             y='Descrição CUCI Seção',
             orientation='h',
             title="Top 5 CUCI por valor total",
-            labels={'Valor US$ FOB': 'Valor comércio anual (USD)', 'Descrição CUCI Seção': ''}
+            labels={'Valor US$ FOB': 'Valor anual (USD)', 'Descrição CUCI Seção': ''}
         )
         grafico_cuci.update_layout(title_x=0.1, yaxis={'categoryorder':'total ascending'})
         st.plotly_chart(grafico_cuci, use_container_width=True)
@@ -131,7 +130,7 @@ with col_graf4:
             y='Descrição ISIC Seção',
             orientation='h',
             title="Top 4 ISIC por valor total",
-            labels={'Valor US$ FOB': 'Valor comércio anual (USD)', 'Descrição ISIC Seção': ''}
+            labels={'Valor US$ FOB': 'Valor anual (USD)', 'Descrição ISIC Seção': ''}
         )
         grafico_isic.update_layout(title_x=0.1, yaxis={'categoryorder':'total ascending'})
         st.plotly_chart(grafico_isic, use_container_width=True)
